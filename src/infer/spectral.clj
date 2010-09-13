7e2019f488631c6ec73b4b3f39a9ed25a22c7199
Hamilton Ulmer <hamiltonulmer@Hamilton-Ulmers-MacBook-Pro-2.local>
2010-09-13 09:01:25 -0700

    Added a new spectral library, and implementing power iteration.  Accompanying it is a small test file.

---
+++ src/infer/spectral.clj
@@ -0,0 +1,20 @@
+(ns infer.spectral
+  "Fundamental spectral machine learning methods.  Power iteration.
+  Spectral clustering.  Spectral ranking."
+  (:use infer.matrix)
+  (:use infer.measures)
+  (:use infer.learning))
+
+(defn power-iter
+  "Computes the eigenvector associated with the largest eigenvalue.
+  Returns a map with keys :eigenvalue, :eigenvector.  The eigenvector
+  Is normalized to have length 1."
+  ([A] (power-iter A (column-matrix (repeat (row-count A) 1))))
+  ([A r] (power-iter A r 0.0000001))
+  ([A r precision]
+  (let [r* (times A r)
+        next-r (divide r* (l2-norm r*))
+        next-lambda (get-at (times (trans next-r) A next-r) 0 0)]
+          (if (euclidean-convergence? precision (from-column-matrix r) (from-column-matrix next-r))
+            {:eigenvalue next-lambda, :eigenvector next-r}
+            (recur A next-r precision)))))
