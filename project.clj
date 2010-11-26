941aa7253147e8cd7ef8f399d96180b9bf5cdc43
aria42 <me@aria42.com>
2010-11-26 14:08:35 -0800

    Moved to Clojure 1.2 final

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject infer "1.0-SNAPSHOT"
   :description "inference and machine learning for clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
 		 [clojure-csv/clojure-csv "1.1.0"]
 		 [org.apache.commons/commons-math "2.0"]
                  [ujmp-complete "0.2.4"]
